25ec0856d52cfc2b5ff74ae8d41e1e055183d8c7
anxlgit <ferpalaciosd@gmail.com>
2017-11-09 11:57:05 +0200

    Remove convoluted favicon approach

--- me.php
+++ me.php
@@ -5,6 +5,7 @@
   <h2>Hi, my name is Anxl.</h2>
   <h2>I make and devour art constantly.</h2>
   <h2><a href="https://twitter.com/anxlacc">Follow me</a>, maybe.</h2>
+  <h2>Or send me <a href="mailto:mail@anxl.faith">a mail</a></h2>
 </section>
 
 <?php include("footer.php");?>
