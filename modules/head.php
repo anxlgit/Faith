7448f52cd56b49c4dfa2c592efc733cdb78694b9
anxlgit <ferpalaciosd@gmail.com>
2017-11-16 17:29:59 +0200

    Fix a conceptual problem when linking the stylesheets

--- modules/head.php
+++ modules/head.php
@@ -9,7 +9,7 @@
   <meta name="author" content="Anxl">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://fonts.googleapis.com/css?family=Sorts+Mill+Goudy:400,400i&amp;subset=latin-ext" rel="stylesheet">
-  <link rel="stylesheet" href="links/css/style.nested.css">
+  <link rel="stylesheet" href="../links/css/style.nested.css">
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
---
+++ projects/swift.php
@@ -0,0 +1,15 @@
+
+<!-- Blog Index -->
+
+<?php include("../modules/head.php");?>
+
+<blockquote class="twitter-tweet" data-lang="es">
+  <p lang="en" dir="ltr">This be the tale I was told. Listen.
+    <br>“Island I” by <a href="https://twitter.com/codycobb?ref_src=twsrc%5Etfw">@codycobb</a>
+    <a href="https://t.co/h5GC7stAS6">pic.twitter.com/h5GC7stAS6</a>
+  </p>&mdash; 𝔉 (@anxlacc)
+  <a href="https://twitter.com/anxlacc/status/869803783659958272?ref_src=twsrc%5Etfw">31 de mayo de 2017</a>
+</blockquote>
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+<?php include("../modules/footer.php");?>
