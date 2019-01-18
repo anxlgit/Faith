68fbb72c6a86684c7f6d971f414c15c257dd6d61
anxlgit <anxlmailacc@gmail.com>
2019-01-18 19:36:46 +0100

    Make into 4.0

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Faith is a personal website / project in which try to apply all knowledge on web
 1.0 was made in PHP/HTML/SASS.
 2.0 was developed in Metalsmith.
 3.0 went back to old school HTML and CSS.
+4.0 added some jQuery to make it flashier.
 
 ![Faith Preview](preview.png)
---
+++ script.js
@@ -0,0 +1,17 @@
+$(function() {
+  // $(document).ready shorthand
+  $("section, img, p").fadeIn("slow"); });
+
+$(document).ready(function() {
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+    /* Check the location of each desired element */
+    $("section, img, p").each(function(i) {
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it it */
+      if (bottom_of_window > bottom_of_object - 150) {
+        $(this).animate({ opacity: "1" }, 1000),
+        $(this).find('.lefttitle').animate({left:"-1px"}, 1500),
+        $(this).find('.righttitle').animate({right:"-5px"}, 1500);
+      } }); }); });
