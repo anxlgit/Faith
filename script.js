64dcbf339476aae5a5827af3e040f50d244e5669
anxlgit <anxlmailacc@gmail.com>
2019-03-25 11:08:41 +0100

    add agency pages

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(function() {
   // $(document).ready shorthand
-  $("section, img, p").fadeIn("slow"); });
+  $("section, img, p, h1, h2, h3, h4, span").fadeIn("slow"); });
 
 $(document).ready(function() {
   /* Every time the window is scrolled ... */
   $(window).scroll(function() {
     /* Check the location of each desired element */
-    $("section, img, p").each(function(i) {
+    $("section, img, p, h1, h2, h3, h4, span").each(function(i) {
       var bottom_of_object = $(this).position().top + $(this).outerHeight();
       var bottom_of_window = $(window).scrollTop() + $(window).height();
       /* If the object is completely visible in the window, fade it it */
-      if (bottom_of_window > bottom_of_object - 150) {
+      if (bottom_of_window > bottom_of_object - 100) {
         $(this).animate({ opacity: "1" }, 1000),
         $(this).find('.lefttitle').animate({left:"-1px"}, 1500),
         $(this).find('.righttitle').animate({right:"-5px"}, 1500);
